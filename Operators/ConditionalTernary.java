df1a0b0ab1b13a0fd580a74882da2440c241f1e0
vedantkhamkar123 <153050950+vedantkhamkar123@users.noreply.github.com>
2023-12-11 13:09:39 +0530

    Create ConditionalTernary.java

---
+++ Operators/ConditionalTernary.java
@@ -0,0 +1,13 @@
+public class ConditionalTernary {
+
+    public static void main(String[] args) {
+            int number = 9;
+            String result = (number % 2 == 0) ? "Even" : "Odd";
+            System.out.println(result); 
+
+    }
+
+}
+
+//                           --------OUTPUT---------
+                                      Odd
