62f9dc7ac95b36c5d8374246163addb03ea6f8d9
vedantkhamkar123 <153050950+vedantkhamkar123@users.noreply.github.com>
2023-12-11 12:10:33 +0530

    Create ArithmeticOperators.java

---
+++ Operators/ArithmeticOperators.java
@@ -0,0 +1,23 @@
+// 1. Arithmetic Operators:
+//    - Addition (+): Adds two operands.
+//    - Subtraction (-): Subtracts the right operand from the left operand.
+//    - Multiplication (*): Multiplies two operands.
+//    - Division (/): Divides the left operand by the right operand.
+//    - Modulus (%): Returns the remainder after division.
+
+public class ArithmeticOperators {
+    public static void main(String[] args) {
+        System.out.println("Arithmetic Operators + "+(45+90));
+        System.out.println("Arithmetic Operators - "+(45-90));
+        System.out.println("Arithmetic Operators * "+(45*90));
+        System.out.println("Arithmetic Operators / "+(4567/9));
+        System.out.println("Arithmetic Operators % "+(4567%9));
+    }
+}
+
+//   ---------OUTPUT---------
+Arithmetic Operators + 135
+Arithmetic Operators - -45
+Arithmetic Operators * 4050
+Arithmetic Operators / 507
+Arithmetic Operators % 4
