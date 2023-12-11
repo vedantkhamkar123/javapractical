a38bbe5fdcfd244413aa90ef480a1b386c16cc8d
vedantkhamkar123 <153050950+vedantkhamkar123@users.noreply.github.com>
2023-12-11 13:02:05 +0530

    Create AssignmentOperators.java

---
+++ Operators/AssignmentOperators.java
@@ -0,0 +1,28 @@
+// 2. Assignment Operators:
+//    - Simple Assignment (=): Assigns a value to a variable.
+//    - Compound Assignment Operators (+=, -=, *=, /=, %=): Performs an arithmetic operation and assigns the result to the left operand.
+
+public class AssignmentOperators {
+    public static void main(String[] args) {
+
+        int num = 10; // Assigns value into num variable using " = "
+        // num = num + 10
+        num += 10;
+        System.out.println(num);
+        // num = num - 10
+        num -= 10;
+        System.out.println(num);
+        // num = num / 10
+        num /= 10;
+        System.out.println(num);
+        // num = num * 10
+        num *= 10;
+        System.out.println(num);
+
+    }
+}
+//    -------OUTPUT------
+  20
+  10
+  1
+  10
