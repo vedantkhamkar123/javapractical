e9bca36c7ab85c05ab06ff4aebfbac5d34b2fe55
vedantkhamkar123 <153050950+vedantkhamkar123@users.noreply.github.com>
2023-12-11 13:14:12 +0530

    Create IncrementDecrementOperators.java

---
+++ Operators/IncrementDecrementOperators.java
@@ -0,0 +1,16 @@
+public class IncrementDecrementOperators {
+    public static void main(String[] args) {
+       int number = 100;
+                //Increment (++)
+                number++;
+                System.out.println(number);
+                //Decrement (--):
+                int number2 = 80;
+                number2--;
+                System.out.println(number2);
+            }
+        }
+
+//    -------OUTPUT--------
+    101
+    79  
