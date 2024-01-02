d27ba13dd0ac812ef20dd55bff7b843fe57711ae
vedantkhamkar123 <153050950+vedantkhamkar123@users.noreply.github.com>
2024-01-02 20:15:53 +0530

    Update Answers.java

--- JavaQuestions/Answers.java
+++ JavaQuestions/Answers.java
@@ -1,1 +1,34 @@
-Write
+1.Write a java program to illustrate different types of data?
+
+package javapractice;
+
+public class Datatype {
+    public static void main(String[] args) {
+
+            byte a = 30;
+            System.out.println("byte var="+a);
+
+            int b = 40;
+            System.out.println("int var="+b);
+
+            char c = 'A';
+            System.out.println("char ="+c);
+
+            long d = 123456;
+            System.out.println("long var="+d);
+
+            float e = 1.5f;
+            System.out.println("float var="+e);
+
+            double f = 1.67999;
+            System.out.println("Double var="+f);
+
+            short g = 2;
+            System.out.println("short var="+g);
+
+            boolean h= true;
+            System.out.println("boolean var="+h);
+
+    }}
+
+
