58848bcf9510e3324639d6857f5ea6cea6bd1f08
vedantkhamkar123 <153050950+vedantkhamkar123@users.noreply.github.com>
2024-01-19 20:22:56 +0530

    Create CharDemo2.java

---
+++ BasicCode/CharDemo2.java
@@ -0,0 +1,9 @@
+class CharDemo2 {
+public static void main(String args[]) {
+char chl;
+chl = 'X';
+System.out.println("chl contains " + ch1);
+chl++; // increment chl
+System.out.println("chl is now" + chl);
+}
+}
