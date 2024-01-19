fc7fef4be5dd60a39d3a1164c01a0451d2ff7a80
vedantkhamkar123 <153050950+vedantkhamkar123@users.noreply.github.com>
2024-01-19 20:13:51 +0530

    Create SquareRoot.java

---
+++ BasicCode/SquareRoot.java
@@ -0,0 +1,10 @@
+import java.lang.Math;
+class SquareRoot {
+public static void main(String args[])
+{
+double x=5: // Declaration and initialization
+double y: // Simple declaration
+y= Math.sqrt(x) ;
+System.out.println("y = "+ y):
+}
+}
