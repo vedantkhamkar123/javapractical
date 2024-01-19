87d0a384b35f375cda32b8caa35f9866697d5c45
vedantkhamkar123 <153050950+vedantkhamkar123@users.noreply.github.com>
2024-01-19 21:09:55 +0530

    Create  BoolLogic.java

---
+++ BasicCode/BoolLogic.java
@@ -0,0 +1,19 @@
+class BoolLogic {
+public static void main(String args[]) {
+booleana a= true;
+boolean b= false;
+boolean c=a|b;
+boolean d=a & b
+booleane e =a^b;
+boolean f= (!a & b) (a & !b);
+boolean g=!a;
+
+System.out.println(" a="+a) ;
+System.out.println(" b="+b) ;
+System.out.println(" a|b="+c) ;
+System.out.println(" a\&b="+d) ;
+System.out.println(" a^b="+e) ;
+System.out.println("!a&b|a&!b="+f) ;
+System.out.println(" !a="+g) ;
+}
+}
