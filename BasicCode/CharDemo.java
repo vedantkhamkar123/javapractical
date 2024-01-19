c92cb59f71dde588177209368ef9743bf7cd327d
vedantkhamkar123 <153050950+vedantkhamkar123@users.noreply.github.com>
2024-01-19 20:21:29 +0530

    Create CharDemo.java

---
+++ BasicCode/CharDemo.java
@@ -0,0 +1,10 @@
+class CharDemo 
+{
+  public static void main(String args[])
+  {
+    char chl, ch2;
+chl = 88; // code for X 
+ch2 =  'Y'; 
+System.out.print("chl and ch2: "); System.out.println(chl + " "+ ch2);
+}
+}
