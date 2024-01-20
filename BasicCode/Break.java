097f0950f4b04760f3ad2318b67b1ebc40f945f4
vedantkhamkar123 <153050950+vedantkhamkar123@users.noreply.github.com>
2024-01-20 19:29:20 +0530

    Create  Break.java

---
+++ BasicCode/Break.java
@@ -0,0 +1,19 @@
+class Break {
+public static void main(String args[]) {
+
+booleant = true;
+first: {
+second: {
+third: {
+System.out.println("Before the break.");
+if(t) break second; // break out of second block
+System.out.println("This won't execute");
+}
+
+System.out.println("This won't execute");
+}
+
+System.out.println("This is after second block.");
+}
+}
+}
